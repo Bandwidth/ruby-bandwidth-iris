eb9e41d3cf1b3c9ccb1531d776ea4a5192212474
jmulford-bandwidth <jmulford@bandwidth.com>
2020-02-05 10:22:13 -0500

    Added remove imported tn orders file

--- lib/ruby-bandwidth-iris.rb
+++ lib/ruby-bandwidth-iris.rb
@@ -28,5 +28,6 @@ require 'bandwidth-iris/tn_reservation'
 require 'bandwidth-iris/user'
 require 'bandwidth-iris/import_tn_orders'
 require 'bandwidth-iris/import_tn_checker'
+require 'bandwidth-iris/remove_imported_tn_orders'
 
 require 'bandwidth-iris/version'
