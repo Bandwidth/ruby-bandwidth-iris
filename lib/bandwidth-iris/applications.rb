558631ca79ead23a214cd39f763c1618580d89b4
jmulford-bandwidth <jmulford@bandwidth.com>
2020-05-12 10:35:12 -0400

    Updated return type to be less confusing to use

--- lib/bandwidth-iris/applications.rb
+++ lib/bandwidth-iris/applications.rb
@@ -7,31 +7,31 @@ module BandwidthIris
 
     def self.get_applications(client)
       data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
-      return data
+      return data[0][:application_list][:application]
     end
     wrap_client_arg :get_applications
 
     def self.create_application(client, application_data)
       data = client.make_request(:post, client.concat_account_path(APPLICATIONS_PATH), {:application => application_data})
-      return data
+      return data[0][:application]
     end
     wrap_client_arg :create_application
 
     def self.get_application(client, application_id)
       data = client.make_request(:get, client.concat_account_path("#{APPLICATIONS_PATH}/#{application_id}"))
-      return data
+      return data[0][:application]
     end
     wrap_client_arg :get_application
 
     def self.partial_update_application(client, application_id, application_data)
       data = client.make_request(:patch, client.concat_account_path("#{APPLICATIONS_PATH}/#{application_id}"), {:application => application_data})
-      return data
+      return data[0][:application]
     end
     wrap_client_arg :partial_update_application
 
     def self.complete_update_application(client, application_id, application_data)
       data = client.make_request(:put, client.concat_account_path("#{APPLICATIONS_PATH}/#{application_id}"), {:application => application_data})
-      return data
+      return data[0][:application]
     end
     wrap_client_arg :complete_update_application
 
@@ -42,7 +42,7 @@ module BandwidthIris
 
     def self.get_application_sippeers(client, application_id)
       data = client.make_request(:get, client.concat_account_path("#{APPLICATIONS_PATH}/#{application_id}/associatedsippeers"))
-      return data
+      return data[0][:associated_sip_peers][:associated_sip_peer]
     end
     wrap_client_arg :get_application_sippeers
   end
--- spec/bandwidth-iris/application_spec.rb
+++ spec/bandwidth-iris/application_spec.rb
@@ -12,22 +12,22 @@ describe BandwidthIris::Applications do
   describe '#get' do
     it 'should get applications' do
       client.stubs.get("/v1.0/accounts/accountId/applications", ){|env| [200, {},  Helper.xml['applicationList']]}
-      application = Applications.get_applications(client)[0]
-      expect(application[:application_list][:application][0][:application_id]).to eql("d1")
-      expect(application[:application_list][:application][1][:application_id]).to eql("d2")
+      applications = Applications.get_applications(client)
+      expect(applications[0][:application_id]).to eql("d1")
+      expect(applications[1][:application_id]).to eql("d2")
     end
 
     it 'should get an application' do
       client.stubs.get("/v1.0/accounts/accountId/applications/id", ){|env| [200, {},  Helper.xml['application']]}
-      application = Applications.get_application(client, "id")[0]
-      expect(application[:application][:application_id]).to eql("d1")
+      application = Applications.get_application(client, "id")
+      expect(application[:application_id]).to eql("d1")
     end
 
     it 'should get an application\'s sippeers' do
       client.stubs.get("/v1.0/accounts/accountId/applications/id/associatedsippeers", ){|env| [200, {},  Helper.xml['applicationSippeers']]}
-      sippeers = Applications.get_application_sippeers(client, "id")[0]
-      expect(sippeers[:associated_sip_peers][:associated_sip_peer][0][:site_id]).to eql(1)
-      expect(sippeers[:associated_sip_peers][:associated_sip_peer][1][:site_id]).to eql(2)
+      sippeers = Applications.get_application_sippeers(client, "id")
+      expect(sippeers[0][:site_id]).to eql(1)
+      expect(sippeers[1][:site_id]).to eql(2)
     end
   end
 
@@ -39,8 +39,8 @@ describe BandwidthIris::Applications do
         :msg_callback_url => "https://test.com"
       }
       client.stubs.post("/v1.0/accounts/accountId/applications", client.build_xml({:application => data})){|env| [200, {},  Helper.xml['application']]}
-      application = Applications.create_application(client, data)[0]
-      expect(application[:application][:application_id]).to eql("d1")
+      application = Applications.create_application(client, data)
+      expect(application[:application_id]).to eql("d1")
     end
   end
 
@@ -52,8 +52,8 @@ describe BandwidthIris::Applications do
         :msg_callback_url => "https://test.com"
       }
       client.stubs.patch("/v1.0/accounts/accountId/applications/id", client.build_xml({:application => data})){|env| [200, {},  Helper.xml['application']]}
-      application = Applications.partial_update_application(client, "id", data)[0]
-      expect(application[:application][:application_id]).to eql("d1")
+      application = Applications.partial_update_application(client, "id", data)
+      expect(application[:application_id]).to eql("d1")
     end
 
     it 'should complete update an application' do
@@ -63,8 +63,8 @@ describe BandwidthIris::Applications do
         :msg_callback_url => "https://test.com"
       }
       client.stubs.put("/v1.0/accounts/accountId/applications/id", client.build_xml({:application => data})){|env| [200, {},  Helper.xml['application']]}
-      application = Applications.complete_update_application(client, "id", data)[0]
-      expect(application[:application][:application_id]).to eql("d1")
+      application = Applications.complete_update_application(client, "id", data)
+      expect(application[:application_id]).to eql("d1")
     end
   end
 
