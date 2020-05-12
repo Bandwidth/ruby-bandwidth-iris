d8c649c9376395eb7afa8c842d424ae3c3ccd7ac
jmulford-bandwidth <jmulford@bandwidth.com>
2020-05-12 11:21:28 -0400

    Added skeleton file

---
+++ lib/bandwidth-iris/sip_peer_products.rb
@@ -0,0 +1,120 @@
+module BandwidthIris
+  SIPPEER_PRODUCTS_PATH = 'products'
+
+  class SipPeerProducts
+    extend ClientWrapper
+    include ApiItem
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
+
+    def self.get_applications(client, site_id, sippeer_id)
+      data = client.make_request(:get, client.concat_account_path(APPLICATIONS_PATH))
+      return data
+    end
+    wrap_client_arg :get_applications
