d5e46baeaf18711194e541541fa516a83d3a9a8f
jmulford-bandwidth <jmulford@bandwidth.com>
2020-05-12 14:26:10 -0400

    Added body parameters

--- lib/bandwidth-iris/sip_peer_products.rb
+++ lib/bandwidth-iris/sip_peer_products.rb
@@ -12,13 +12,13 @@ module BandwidthIris
     wrap_client_arg :get_origination_settings
 
     def self.set_origination_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/origination/settings")
+      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/origination/settings", {:sip_peer_origination_settings => data})
       return data
     end
     wrap_client_arg :set_origination_settings
 
     def self.update_origination_settings(client, site_id, sippeer_id, data)
-      client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/origination/settings")
+      client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/origination/settings", {:sip_peer_origination_settings => data})
     end
     wrap_client_arg :update_origination_settings
 
@@ -29,13 +29,13 @@ module BandwidthIris
     wrap_client_arg :get_termination_settings
 
     def self.create_termination_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/termination/settings")
+      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/termination/settings", {:sip_peer_termination_settings => data})
       return data
     end
     wrap_client_arg :create_termination_settings
 
     def self.update_termination_settings(client, site_id, sippeer_id, data)
-      client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/termination/settings")
+      client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/termination/settings", {:sip_peer_termination_settings => data})
     end
     wrap_client_arg :update_termination_settings
 
@@ -46,13 +46,13 @@ module BandwidthIris
     wrap_client_arg :get_sms_feature_settings
 
     def self.create_sms_feature_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/sms")
+      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/sms", {:sip_peer_sms_feature => data})
       return data
     end
     wrap_client_arg :create_sms_feature_settings
 
     def self.update_sms_feature_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/sms")
+      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/sms", {:sip_peer_sms_feature => data})
       return data
     end
     wrap_client_arg :update_sms_feature_settings
@@ -70,13 +70,13 @@ module BandwidthIris
     wrap_client_arg :get_mms_feature_settings
 
     def self.create_mms_feature_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/mms")
+      data = client.make_request(:post, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/mms", {:mms_feature => data})
       return data
     end
     wrap_client_arg :create_mms_feature_settings
 
     def self.update_mms_feature_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/mms")
+      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/features/mms", {:mms_feature => data})
       return data
     end
     wrap_client_arg :update_mms_feature_settings
@@ -101,7 +101,7 @@ module BandwidthIris
     wrap_client_arg :get_messaging_application_settings
 
     def self.update_messaging_application_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/applicationSettings")
+      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/applicationSettings", {:applications_settings => data})
       return data
     end
     wrap_client_arg :update_messaging_application_settings
@@ -113,7 +113,7 @@ module BandwidthIris
     wrap_client_arg :get_messaging_settings
 
     def self.update_messaging_settings(client, site_id, sippeer_id, data)
-      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/settings")
+      data = client.make_request(:put, "#{client.concat_account_path(SITE_PATH)}/#{site_id}/#{SIPPEER_PATH}/#{sippeer_id}/#{SIPPEER_PRODUCTS_PATH}/messaging/settings", {:sip_peer_messaging_settings => data})
       return data
     end
     wrap_client_arg :update_messaging_settings
