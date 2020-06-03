4bf5520cd93cb95ac5d8e598470bbcfa2a50191c
jmulford-bandwidth <jmulford@bandwidth.com>
2020-06-03 14:22:12 -0400

    Fixed xml and response for get orders

--- lib/bandwidth-iris/emergency_notification_groups.rb
+++ lib/bandwidth-iris/emergency_notification_groups.rb
@@ -8,7 +8,7 @@ module BandwidthIris
 
     def self.create_emergency_notification_group_order(client, data)
       response = client.make_request(:post, "#{client.concat_account_path(ENG_ORDERS_PATH)}", {:emergency_notification_group_order => data})
-      return response[0]
+      return response[0][:emergency_notification_endpoint_order]
     end
     wrap_client_arg :create_emergency_notification_group_order
 
@@ -20,7 +20,7 @@ module BandwidthIris
 
     def self.get_emergency_notification_group_order(client, order_id)
       response = client.make_request(:get, "#{client.concat_account_path(ENG_ORDERS_PATH)}/#{order_id}")
-      return response[0]
+      return response[0][:emergency_notification_endpoint_order]
     end
     wrap_client_arg :get_emergency_notification_group_order
 
